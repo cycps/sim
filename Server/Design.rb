e0dc909550c938b7cbbdef3d1b86f6ac129ece3e
Ryan Goodfellow <rgoodfel@isi.edu>
2015-03-25 16:54:18 -0700

    working toward opts for server

--- Server/Design.rb
+++ Server/Design.rb
@@ -1,6 +1,15 @@
 # Cypress web-server /Design web-app
 
+require 'trollop'
 require 'sinatra'
+
+opts = Trollop::options do
+  opt :data_dir, 
+      "Directory where experiment code repositories are placed",
+      :type => :string,
+      :default => "/var/lib/cypress/"
+
+p opts
 
 post '/Design/New' do
   "Creating #{params[:name]} ...\r\n"
