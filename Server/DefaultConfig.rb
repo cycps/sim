882e2bf66b90d30b50e6a216f37d2d90eb8363f2
Ryan Goodfellow <rgoodfel@isi.edu>
2015-03-27 08:04:43 -0700

    working toward a nice CypressServerd

---
+++ Server/DefaultConfig.rb
@@ -0,0 +1,7 @@
+#defualt Cypress App Server Config
+
+module Cypress
+  module ServerConfig
+    DATA = { :base_dir => '/var/lib/cypress' } 
+  end
+end
--- Server/Design.rb
+++ Server/Design.rb
@@ -1,8 +1,36 @@
 # Cypress web-server /Design web-app
 
-require 'trollop'
 require 'sinatra'
 require 'fileutils'
+require './DefaultConfig.rb'
+
+class CyConfig
+  def check
+    check_acct
+    check_data_dir
+  end
+
+  def check_acct
+    begin
+      cyp_uid = Process::UID.from_name("cypress")
+    rescue
+      puts "cypress user account does not exist, creating it"
+    end
+  end
+
+  def check_data_dir
+    ddir = Cypress::ServerConfig::DATA[:base_dir]
+    if not Dir.exists? ddir 
+      puts "Data directory #{ddir} does not exist, creating it"
+      #FileUtils.mkdir_p ddir
+      #TODO need to create a cypress user + permissions
+      #
+      # 1: detect user, if not cyp switch to cyp if no cyp create and switch
+      # 2: no make dirs and mod them for use with cyp
+      `sudo mkdir -p #{ddir}`
+    end
+  end
+end
 
 class Design
   def new_exp(params)
@@ -19,27 +47,13 @@ class DesignApp < Sinatra::Base
 
 end
 
-def init(opts)
-  #TODO need to ask permission to eleveate to make this dir if necessary
-  ddir = opts[:data_dir]
-  if not Dir.exists? ddir
-    puts "Data directory #{ddir} does not exist, creating it"
-    FileUtils.mkdir_p ddir
-  end
-end
-
 
 def main
-  opts = Trollop::options do
-    opt :data_dir, 
-        "Directory where experiment code repositories are placed",
-        :type => :string,
-        :default => "/var/lib/cypress/"
-  end
-  p opts
-  init opts
-  app = DesignApp.new
-  app.run!
+  conf = CyConfig.new
+  conf.check
+
+  #app = DesignApp.new
+  #app.run!
 end
 
 main
